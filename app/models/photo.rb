b364d4e38c74f7a7700b7d0971967de2aa8c9e8f
Raghu Betina <raghu@firstdraft.com>
2017-01-10 03:24:58 +0000

    Direct association between photosets and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  belongs_to :photoset
+
   # Indirect associations
 
   # Validations
--- app/models/photoset.rb
+++ app/models/photoset.rb
@@ -1,6 +1,9 @@
 class Photoset < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :dependent => :destroy
+
   has_many   :reviews,
              :dependent => :destroy
 
