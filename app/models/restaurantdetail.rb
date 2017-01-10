177e393a87606f36924995699486f68c56d5436c
Raghu Betina <raghu@firstdraft.com>
2017-01-10 03:24:58 +0000

    Direct association between restaurantdetails and restuarants

--- app/models/restaurantdetail.rb
+++ app/models/restaurantdetail.rb
@@ -1,6 +1,10 @@
 class Restaurantdetail < ApplicationRecord
   # Direct associations
 
+  has_many   :restuarants,
+             :foreign_key => "details_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/restuarant.rb
+++ app/models/restuarant.rb
@@ -1,6 +1,9 @@
 class Restuarant < ApplicationRecord
   # Direct associations
 
+  belongs_to :details,
+             :class_name => "Restaurantdetail"
+
   # Indirect associations
 
   # Validations
