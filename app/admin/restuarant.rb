3bbf845b9b2758bca57ba4999b2e87c3a2d41f94
Raghu Betina <raghu@firstdraft.com>
2017-01-10 03:25:13 +0000

    Whitelist columns in Restuarant for ActiveAdmin

--- app/admin/restuarant.rb
+++ app/admin/restuarant.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Restuarant do
 
+ permit_params :name, :rating, :number_of_review, :details_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
